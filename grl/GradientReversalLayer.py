d5d1e27105dcee32ae258bcaaa8763afecf12b4d
Chen-Dixi <cdxcqu2014@163.com>
2019-03-27 19:59:14 +0800

    first commit

---
+++ grl/GradientReversalLayer.py
@@ -0,0 +1,17 @@
+from torch.autograd import Function as function
+#import torch.autograd.Function as function 这样是不正确的，Function是一个类，python这样的语法是import Module，Function要和torch.autograd分开写
+
+class GradientReversalLayer(function):
+
+    @staticmethod
+    def farward(ctx,x,lambda_p):
+        ctx.lambda_p = lambda_p
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx,grad_output):
+        grad_output = grad_output.neg() * ctx.lambda_p
+        return grad_output , None # return as many tnesors as input to forward
+
+    def grad_reverse(x,lambda_p):
+        return GradientReersalLayer.apply(x,lambda_p)
---
+++ models/models.py
@@ -0,0 +1,139 @@
+
+from grl.GradientReversalLayer import GradientReversalLayer
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+class Feature_Extractor(nn.Module):
+
+    def __init__(self):
+        super(Feature_Extractor, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 48, kernel_size=5)
+      
+        self.conv2_drop = nn.Dropout2d()
+
+    def forward(self, input):
+        input = input.expand(input.size()[0], 3, 28, 28)
+       
+        x = F.relu(F.max_pool2d(self.conv1(input), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 48 * 4 * 4)
+
+        return x
+
+class Class_classifier(nn.Module):
+
+    def __init__(self):
+        super(Class_classifier, self).__init__()
+        
+        self.fc1 = nn.Linear(48 * 4 * 4, 100)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, input):
+        # logits = F.relu(self.bn1(self.fc1(input)))
+        # logits = self.fc2(F.dropout(logits))
+        # logits = F.relu(self.bn2(logits))
+        # logits = self.fc3(logits)
+        logits = F.relu(self.fc1(input))
+        logits = self.fc2(F.dropout(logits))
+        logits = F.relu(logits)
+        logits = self.fc3(logits)
+
+        return F.log_softmax(logits, 1)
+
+class Domain_classifier(nn.Module):
+
+    def __init__(self):
+        super(Domain_classifier, self).__init__()
+        # self.fc1 = nn.Linear(50 * 4 * 4, 100)
+        # self.bn1 = nn.BatchNorm1d(100)
+        # self.fc2 = nn.Linear(100, 2)
+        self.fc1 = nn.Linear(48 * 4 * 4, 100)
+        # one nerve cell
+        self.fc2 = nn.Linear(100, 1)
+
+    def forward(self, input, lambda_p):
+        input = GradientReversalLayer.apply(input, lambda_p)
+        # logits = F.relu(self.bn1(self.fc1(input)))
+        # logits = F.log_softmax(self.fc2(logits), 1)
+        logits = F.relu(self.fc1(input))
+        logits = F.sigmoid(self.fc2(logits))
+
+        return logits
+
+
+
+class SVHN_Extractor(nn.Module):
+
+    def __init__(self):
+        super(SVHN_Extractor, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size= 5)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size= 5)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size= 5, padding= 2)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv3_drop = nn.Dropout2d()
+        self.init_params()
+
+    def init_params(self):
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode= 'fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            if isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+
+    def forward(self, input):
+        input = input.expand(input.data.shape[0], 3, 28, 28)
+        x = F.relu(self.bn1(self.conv1(input)))
+        x = F.max_pool2d(x, 3, 2)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 3, 2)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv3_drop(x)
+
+        return x.view(-1, 128 * 3 * 3)
+
+class SVHN_Class_classifier(nn.Module):
+
+    def __init__(self):
+        super(SVHN_Class_classifier, self).__init__()
+        self.fc1 = nn.Linear(128 * 3 * 3, 3072)
+        self.bn1 = nn.BatchNorm1d(3072)
+        self.fc2 = nn.Linear(3072, 2048)
+        self.bn2 = nn.BatchNorm1d(2048)
+        self.fc3 = nn.Linear(2048, 10)
+
+    def forward(self, input):
+        logits = F.relu(self.bn1(self.fc1(input)))
+        logits = F.dropout(logits)
+        logits = F.relu(self.bn2(self.fc2(logits)))
+        logits = self.fc3(logits)
+
+        return F.log_softmax(logits, 1)
+
+class SVHN_Domain_classifier(nn.Module):
+
+    def __init__(self):
+        super(SVHN_Domain_classifier, self).__init__()
+        self.fc1 = nn.Linear(128 * 3 * 3, 1024)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.bn2 = nn.BatchNorm1d(1024)
+        self.fc3 = nn.Linear(1024, 2)
+
+    def forward(self, input, constant):
+        input = GradReverse.grad_reverse(input, constant)
+        logits = F.relu(self.bn1(self.fc1(input)))
+        logits = F.dropout(logits)
+        logits = F.relu(self.bn2(self.fc2(logits)))
+        logits = F.dropout(logits)
+        logits = self.fc3(logits)
+
+        return F.log_softmax(logits, 1)
